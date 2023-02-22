dd2db7bf5df73f9262e40cbb94e67a74817c10df
lingosandi <lai@lingo3d.com>
2023-02-22 20:08:58 +0800

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { Cube, World } from "lingo3d-react"
+import { Cube, LingoEditor, World } from "lingo3d-react"
 import "./App.css"
 
 const App = () => {
   return (
     <World>
-      <Cube animation={{ rotationY: [0, 180, 360] }} />
+      <LingoEditor />
+      <Cube />
     </World>
   )
 }
